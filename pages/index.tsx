c843f17a30e3e4db8cc4ae3adacfa6c92273baf0
Mukesh Yadav <mky773874@gmail.com>
2025-02-28 14:12:29 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,7 @@ interface PokemonAPIResponse {
 function debounce<T extends (...args: unknown[]) => void>(func: T, delay: number): (...args: Parameters<T>) => void {
   let timeoutId: NodeJS.Timeout;
   return (...args: Parameters<T>) => {
-   
-      clearTimeout(timeoutId);
-    
+    clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
       func(...args);
     }, delay);
@@ -98,15 +96,40 @@ export default function Home() {
       <h2 className="text-3xl font-bold text-center text-green-600 mb-8">
         Pokemon Explorer
       </h2>
-      <div className="text-center mb-10">
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="focus:outline-none rounded-sm border px-14 py-1"
-          placeholder="Search By Pokemon Name"
-          type="text"
-        />
+
+      {/* Updated SearchBox */}
+      <div className="max-w-sm mx-auto mb-10">
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-3.5">
+            <svg
+              className="shrink-0 size-4 text-gray-400 dark:text-white/60"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </svg>
+          </div>
+          <input
+            value={name}
+           
+            onChange={(e) => setName(e.target.value)}
+            className="py-3 ps-10 pe-4 outline-none border-2 block w-full border-gray-200 rounded-lg text-sm focus:border-green-500 focus:ring-green-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 drop-shadow-2xl"
+            type="text"
+            placeholder="Search By Pokemon Name"
+            aria-expanded="false"
+            data-hs-combo-box-input=""
+          />
+        </div>
       </div>
+      
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {filteredPokemons.length > 0 ? (
           filteredPokemons.map((pokemon, index) => (
@@ -117,8 +140,9 @@ export default function Home() {
               <Link href={`/${pokemon.name}`}>
                 <Image 
                   src={pokemon.image} 
+                  style={{filter: "drop-shadow(2px 4px 6px black)"}}
                   alt={pokemon.name} 
-                  className="w-full h-40 object-contain mb-4 scale-y-100"
+                  className="w-full drop-shadow-2xl h-40 object-contain mb-4 scale-y-100"
                   width={200} // Adjust the width and height as needed
                   height={200}
                   priority
@@ -146,8 +170,8 @@ export default function Home() {
         <span>Page {currentPage}</span>
         <button
           onClick={() => handlePageChange(nextPageUrl, 'next')}
-          disabled={!nextPageUrl}
-          className={`px-4 py-2 bg-gray-300 rounded ${!nextPageUrl ? 'cursor-not-allowed opacity-50' : 'hover:bg-gray-400'}`}
+          disabled={!nextPageUrl || filteredPokemons.length <= 7}
+          className={`px-4 py-2 bg-gray-300 rounded ${!nextPageUrl || filteredPokemons.length <= 7 ? 'cursor-not-allowed opacity-50' : 'hover:bg-gray-400'}`}
         >
           Next
         </button>
